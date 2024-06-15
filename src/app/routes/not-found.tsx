ecce77b77fb894b61906eec02233fa698accca13
mrrobotoe <aldo.martinez@my.utsa.edu>
2024-06-14 20:46:01 -0500

    added redirects for netlify

--- src/app/routes/not-found.tsx
+++ src/app/routes/not-found.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
-
+import './not-found.styles.scss';
 export const NotFoundRoute = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <Link to="/" replace>
+    <div className="notfound_layout">
+      <h1 className="notfound__header">404 Not Found</h1>
+      <Link className="notfound__link" to="/" replace>
         Go to home
       </Link>
     </div>
